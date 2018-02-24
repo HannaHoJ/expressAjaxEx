65c633f8604a8ddd461892b1d6e836f93c5bad99
Johanna D <johanna.dietzz@gmail.com>
2018-02-24 16:28:26 +0100

    ajax jquery express todo app example

---
+++ server.js
@@ -0,0 +1,58 @@
+// GET		/todos			>> Get all of the todos
+// POST		/todos			>> Create a single todo
+// DELETE	/todos/:todo_id	>> Delete a single todo
+
+
+var express = require('express');
+//get http and https servers
+//var https = require('https');
+var http = require('http');
+var path = require('path');
+//parse json request bodies
+var bodyParser = require('body-parser')
+
+
+var app = express();
+var port = process.env.PORT || 5000;
+
+
+//use bodyParser as middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+//creates error
+//app.use(bodyParser.json());
+
+//importent have index.js ??????Why????
+//https://stackoverflow.com/questions/39912609/how-to-include-static-files-in-express-js
+app.use(express.static(__dirname + '/src'));
+
+//middleware function at /
+app.use('/', (req, res, next) =>{
+	//in case you want to add middleware.
+	next();
+})
+//serve index.html as file
+app.get('/', (req,res) => {
+	//https://stackoverflow.com/questions/25463423/res-sendfile-absolute-path
+  	res.sendFile(path.join(__dirname, './src', 'index.html'));
+});
+
+
+//https://stackoverflow.com/questions/7042340/error-cant-set-headers-after-they-are-sent-to-the-client
+app.post('/:item', function (req, res) {
+	console.log('params: ' + JSON.stringify(req.params));
+	console.log('body: ' + JSON.stringify(req.body));
+  	res.json(req.params);
+  	res.redirect('/');
+  	res.end();
+
+})
+
+// app.delete('/item', function (req, res) {
+// 	res.status(200);
+// 	res.setHeader('Content-type', 'text/json');	
+// 	res.json(req.body);		
+//   	return res.send('Got a DELETE request at /item');
+// })
+
+http.createServer(app).listen(port);
+
